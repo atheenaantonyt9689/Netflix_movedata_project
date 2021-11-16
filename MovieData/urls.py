5f173d526f9c33e2cdb30b2ad408bc5c4bd7061d
Atheena <atheenaantonyt@gmail.com>
2021-11-16 23:02:09 +0530

    movie data filtering

--- MovieData/urls.py
+++ MovieData/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 
-from .views import HomePageView
-
+from .views import NetflixMovieDataview
 urlpatterns = [
-path('', HomePageView.as_view(), name='home'),
+path('', NetflixMovieDataview.as_view(), name='home'),
 ]
--- MovieData/views.py
+++ MovieData/views.py
@@ -1,6 +1,50 @@
 from django.shortcuts import render
-from django.views.generic.base import TemplateView
+from django import views
+from django.views.generic.base import View
+from django.urls import reverse, reverse_lazy
+
+from MovieData.models import NetflixMovieData
 
 # Create your views here.
-class HomePageView(TemplateView):    
+class NetflixMovieDataview(View ):
+
+    model= NetflixMovieData
+
     template_name = 'movie/index.html'
+    success_url = reverse_lazy('home')
+
+
+    def get(self,request,*args,**kwargs):   
+       
+        genre_data=NetflixMovieData.objects.values('genre',).order_by('genre',).distinct()
+        language_data=NetflixMovieData.objects.values('lanaguage',).order_by('lanaguage').distinct()
+       
+        context={
+              "genre_data":genre_data,
+              "language_data":language_data
+
+        }           
+        return render(request,self.template_name,context)
+
+    def post(self,request,*args,**kwargs): 
+        if request.method == "POST":            
+
+        # print(request.POST.get('genre'))
+            genre=request.POST.get('genre')
+            language=request.POST.get('lanaguage') 
+            # print("helloooo form posttt       ",genre,language)       
+            search_result = NetflixMovieData.objects.filter(genre=genre,lanaguage=language)
+            count_search_result = search_result.count() 
+            # print(search_result)
+       
+        context={
+           "search_result": search_result,
+           "count_search_result": count_search_result
+
+        }           
+        return render(request,self.template_name,context)
+
+
+
+
+
